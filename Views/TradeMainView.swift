55e16ad98fcb0c34b8c1ca977ae8f058d2dd2427
yukkod1 <chotkij1@mail.ru>
2023-10-16 17:19:22 +0200

    last update

---
+++ Views/TradeMainView.swift
@@ -0,0 +1,25 @@
+//
+//  TradeMainView.swift
+//  TradeApp
+//
+//  Created by Artem Cherkasov on 03/09/2023.
+//
+
+import UIKit
+
+class TradeMainView: UIView {
+    
+   
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
